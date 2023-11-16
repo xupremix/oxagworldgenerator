207baa0496f5281f211dfc741451ed077bc5ca3a
Filippo Lollato <95566781+xupremix@users.noreply.github.com>
2023-11-16 14:38:48 +0100

    Initial commit

---
+++ target/debug/build/rustversion-cffa5e5b392b3232/build_script_build-cffa5e5b392b3232.d
@@ -0,0 +1,6 @@
+C:\Users\lolla\RustroverProjects\lib_oxidizing_agents\target\debug\build\rustversion-cffa5e5b392b3232\build_script_build-cffa5e5b392b3232.exe: C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rustversion-1.0.14\build/build.rs C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rustversion-1.0.14\build\rustc.rs
+
+C:\Users\lolla\RustroverProjects\lib_oxidizing_agents\target\debug\build\rustversion-cffa5e5b392b3232\build_script_build-cffa5e5b392b3232.d: C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rustversion-1.0.14\build/build.rs C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rustversion-1.0.14\build\rustc.rs
+
+C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rustversion-1.0.14\build/build.rs:
+C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rustversion-1.0.14\build\rustc.rs:
---
+++ target/debug/deps/unicode_ident-e620663772872001.d
@@ -0,0 +1,8 @@
+C:\Users\lolla\RustroverProjects\lib_oxidizing_agents\target\debug\deps\unicode_ident-e620663772872001.rmeta: C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
+
+C:\Users\lolla\RustroverProjects\lib_oxidizing_agents\target\debug\deps\libunicode_ident-e620663772872001.rlib: C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
+
+C:\Users\lolla\RustroverProjects\lib_oxidizing_agents\target\debug\deps\unicode_ident-e620663772872001.d: C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
+
+C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs:
+C:\Users\lolla\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs:
