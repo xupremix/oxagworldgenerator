846bb54154589c9e4a05d94d550c02eb9c68b9d3
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-11-26 04:54:17 +0100

    feat: lib and util folders refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+//! Oxidizing Agents library
+//!
+//! This library provides:
+//! - a world generator
+//! - a tool
+
+#![warn(missing_docs)]
+
 mod test;
 pub mod tools;
 pub mod utils;
-pub mod worldgenerator;
+pub mod world_generator;
---
+++ src/utils/errors.rs
@@ -0,0 +1,19 @@
+use robotics_lib::world::tile::Content;
+
+// TODO: Describe errors
+#[derive(Debug, Clone)]
+pub enum OxAgError {
+    SizeNotSet,
+    SeedNotSet,
+    WorldOptionsNotSet,
+    ContentOptionsNotSet,
+    WeatherOptionsNotSet,
+    InvalidWorldGenerationOption,
+    InvalidContentGenerationOption(Content),
+    ContentOptionNotSet(Content),
+    CannotSetContentOptionForNone,
+    WrongLowerBound,
+    WrongUpperBound,
+    InvalidSpawnLevel(Content),
+    RangesAreOutOfBounds,
+}
