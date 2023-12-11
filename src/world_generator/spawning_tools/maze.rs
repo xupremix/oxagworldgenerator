90d1860b964cfa2b66bf1cc8344d3a97dab3a2e6
OmegaEnergy2058 <marco.basso@unitn.it>
2023-12-11 16:11:09 +0100

    Random tile circle

--- src/world_generator/spawning_tools/maze.rs
+++ src/world_generator/spawning_tools/maze.rs
@@ -3,9 +3,7 @@ use crate::world_generator::spawning_tools::MazeBuilder;
 use rand::prelude::StdRng;
 use rand::seq::SliceRandom;
 use rand::{Rng, SeedableRng};
-use robotics_lib::world::tile::TileType::Grass;
 use robotics_lib::world::tile::{Content, Tile, TileType};
-use serde::de::Unexpected::Option;
 
 pub(crate) fn maze_builder_init(seed: u64, mut size: usize) -> MazeBuilder {
     MazeBuilder {
@@ -25,16 +23,31 @@ pub(crate) fn maze_builder_init(seed: u64, mut size: usize) -> MazeBuilder {
     }
 }
 
+pub(crate) fn random_tile(rng: &mut StdRng) -> Vec<TileType> {
+    let mut tile_type = vec![
+        TileType::Grass,
+        TileType::ShallowWater,
+        TileType::Sand,
+        TileType::Snow,
+    ];
+    tile_type.shuffle(rng);
+    tile_type
+}
+
 impl MazeBuilder {
     // maze builder
     pub(crate) fn builder(mut self) -> (Vec<Vec<Tile>>, (usize, usize)) {
-        let mut rng = &mut StdRng::seed_from_u64(self.seed);
+        let rng = &mut StdRng::seed_from_u64(self.seed);
         let (mut spawn_x, mut spawn_y) = self.random_point(rng);
         self.maze_builder_loop(spawn_x as i32, spawn_y as i32, rng);
-        let n_circle = self.size as f32;
 
-        for _ in (0..(self.size as f32 * 0.3) as usize) {
-            self.random_circles(rng);
+        let mut tile_type = random_tile(rng);
+        for _ in 0..(self.size as f32 * 0.3) as usize {
+            if tile_type.len() <= 0 {
+                tile_type = random_tile(rng);
+            }
+            let tile = tile_type.pop().unwrap();
+            self.random_circles(rng, tile);
         }
         self.teleport_spawner(rng);
 
@@ -48,20 +61,32 @@ impl MazeBuilder {
             return;
         }
         for _ in 0..max as usize {
-            let (mut x, mut y) = self.random_point(rng);
-            while self.map[x][y].tile_type == TileType::Wall {
-                (x, y) = self.random_point(rng);
-            }
-            self.map[x][y].tile_type = TileType::Teleport(false);
+            let (x, y) = self.random_not_street(rng);
+            self.map[y][x].content = Content::JollyBlock(1);
         }
     }
+
+    fn random_not_street(&self, rng: &mut StdRng) -> (usize, usize) {
+        let (mut x, mut y) = (
+            rng.gen_range(1..self.size - 1),
+            rng.gen_range(1..self.size - 1),
+        );
+        while self.map[y][x].tile_type != TileType::Street {
+            (x, y) = (
+                rng.gen_range(1..self.size - 1),
+                rng.gen_range(1..self.size - 1),
+            );
+        }
+        (x, y)
+    }
+
     // Path setter
     fn set_path(&mut self, x: usize, y: usize) {
         self.map[y][x].tile_type = TileType::Street;
     }
     // Check if where i want to place a path is wall
     fn is_wall(&self, x: usize, y: usize) -> TileType {
-        if (0 < x && x < self.size && 0 < y && y < self.size) {
+        if 0 < x && x < self.size && 0 < y && y < self.size {
             self.map[y][x].tile_type
         } else {
             TileType::Street
@@ -79,9 +104,9 @@ impl MazeBuilder {
     //Spawn point need to be in an odd position
     fn check_odd(&self, mut num: usize) -> usize {
         if num % 2 == 0 {
-            if (num + 1 < self.size) {
+            if num + 1 < self.size {
                 num += 1;
-            } else if (num - 1 > 0) {
+            } else if num - 1 > 0 {
                 num -= 1
             }
         }
@@ -94,6 +119,27 @@ impl MazeBuilder {
         } else {
             false
         }
+    }
+
+    fn spawn_end(&mut self, rng: &mut StdRng) {
+        let (x, y) = self.random_not_street(rng);
+        self.map[y][x].content = Content::JollyBlock(1);
+    }
+
+    fn random_circles(&mut self, rng: &mut StdRng, tile: TileType) {
+        let (spawn_x, spawn_y) = self.random_point(rng);
+        let size = self.size as f32;
+        let radius = rng.gen_range(1..size.sqrt() as usize);
+        spawn_circle(
+            &mut self.map,
+            rng,
+            self.size,
+            spawn_x,
+            spawn_y,
+            radius,
+            &(None, Some(tile)),
+        );
+        self.map[spawn_x][spawn_y].tile_type = TileType::Teleport(false);
     }
 
     fn maze_builder_loop(&mut self, start_x: i32, start_y: i32, mut rng: &mut StdRng) {
@@ -129,35 +175,5 @@ impl MazeBuilder {
                 }
             }
         }
-    }
-
-    fn spawn_end(&mut self, rng: &mut StdRng) {
-        let (mut x, mut y) = (
-            rng.gen_range(1..self.size - 1),
-            rng.gen_range(1..self.size - 1),
-        );
-        while self.map[y][x].tile_type != TileType::Street {
-            (x, y) = (
-                rng.gen_range(1..self.size - 1),
-                rng.gen_range(1..self.size - 1),
-            );
-        }
-        self.map[y][x].content = Content::JollyBlock(1);
-    }
-
-    fn random_circles(&mut self, rng: &mut StdRng) {
-        let (spawn_x, spawn_y) = self.random_point(rng);
-        let size = self.size as f32;
-        let radius = rng.gen_range(1..size.sqrt() as usize);
-        spawn_circle(
-            &mut self.map,
-            rng,
-            self.size,
-            spawn_x,
-            spawn_y,
-            radius,
-            &(None, Some(Grass)),
-        );
-        self.map[spawn_x][spawn_y].tile_type = TileType::Teleport(false);
     }
 }
